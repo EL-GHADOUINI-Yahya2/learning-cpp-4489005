6529facd081f2b0eaf38741ebc2f5a6376d34c97
EL-GHADOUINI-Yahya2 <54476475+EL-GHADOUINI-Yahya2@users.noreply.github.com>
2024-05-05 19:56:49 +0000

    updated program with solution

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,18 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+#include <string>
+
+int main() {
+    using namespace std;
+string name;
+
+cout<<"What is your name ?"<<endl;
+cin>>name;
+cout<<"Hello " + name<<endl;
+
+cout<<endl<<endl;
+return 0;
+}
